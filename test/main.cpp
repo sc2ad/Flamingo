197c96b32efa9e374ef3d155c4514de80cb9c95b
Adam ? <adamznow@gmail.com>
2024-07-21 20:34:03 -0700

    Add test-wrapper.hpp and add self-checking validation to fixups and hooks
    Ideally should be split up even further, but this will do for a start

--- test/main.cpp
+++ test/main.cpp
@@ -1,18 +1,23 @@
 // Main test runner for testing fixups behave as intended
+#include <capstone/arm64.h>
+#include <fmt/compile.h>
+#include <fmt/core.h>
+#include <fmt/format.h>
 #include <string.h>
 #include <sys/mman.h>
 #include <array>
 #include <cstddef>
 #include <cstdint>
 #include <cstdio>
+#include <cstdlib>
 #include <span>
 
 #include "../shared/fixups.hpp"
 #include "../shared/page-allocator.hpp"
 #include "capstone/capstone.h"
+#include "test-wrapper.hpp"
 
-
-void print_decode_loop(std::span<uint32_t> data) {
+static void print_decode_loop(std::span<uint32_t> data) {
   auto handle = flamingo::getHandle();
   for (size_t i = 0; i < data.size(); i++) {
     cs_insn* insns = nullptr;
@@ -26,7 +31,7 @@ void print_decode_loop(std::span<uint32_t> data) {
   }
 }
 
-decltype(auto) test_near(uint32_t* target, [[maybe_unused]] uint32_t const* callback) {
+static decltype(auto) test_near(uint32_t* target, [[maybe_unused]] uint32_t const* callback) {
   constexpr size_t hookSizeNumInsts = 5;
   constexpr size_t trampolineSize = 64;
   static std::array<uint32_t, trampolineSize> test_trampoline;
@@ -44,7 +49,7 @@ decltype(auto) test_near(uint32_t* target, [[maybe_unused]] uint32_t const* call
   return fixups;
 }
 
-void perform_near_hook_test(std::span<uint8_t> to_hook) {
+static auto perform_near_hook_test(std::span<uint8_t> to_hook) {
   std::span<uint32_t> hook_span = std::span<uint32_t>(reinterpret_cast<uint32_t*>(&to_hook[0]),
                                                       reinterpret_cast<uint32_t*>(&to_hook[to_hook.size()]));
   printf("TO HOOK: %p\n", to_hook.data());
@@ -55,9 +60,10 @@ void perform_near_hook_test(std::span<uint8_t> to_hook) {
   print_decode_loop(trampoline_data.fixup_inst_destination.addr);
   puts("HOOKED:");
   print_decode_loop(hook_span);
+  return trampoline_data;
 }
 
-decltype(auto) test_far(uint32_t* target, [[maybe_unused]] uint32_t const* callback) {
+static decltype(auto) test_far(uint32_t* target, [[maybe_unused]] uint32_t const* callback) {
   constexpr size_t hookSizeNumInsts = 5;
   constexpr size_t trampolineSize = 64;
   // We allocate the page with r-x perms, we will mark it as writable when we do the writes and otherwise put it back to
@@ -77,54 +83,217 @@ decltype(auto) test_far(uint32_t* target, [[maybe_unused]] uint32_t const* callb
   return fixups;
 }
 
-void perform_far_hook_test(std::span<uint8_t> to_hook) {
+static auto perform_far_hook_test(std::span<uint8_t> to_hook) {
   std::span<uint32_t> hook_span = std::span<uint32_t>(reinterpret_cast<uint32_t*>(&to_hook[0]),
                                                       reinterpret_cast<uint32_t*>(&to_hook[to_hook.size()]));
   printf("TO HOOK: %p\n", to_hook.data());
   print_decode_loop(hook_span);
   puts("TEST FAR...");
-  auto trampoline = test_far(hook_span.data(), (uint32_t const*)(0xDEADBEEFBAADF00DULL));
+  auto fixups = test_far(hook_span.data(), (uint32_t const*)(0xDEADBEEFBAADF00DULL));
   // Use 20 here as a reasonable guesstimate
-  print_decode_loop(trampoline.fixup_inst_destination.addr);
+  print_decode_loop(fixups.fixup_inst_destination.addr);
   puts("HOOKED:");
   print_decode_loop(hook_span);
+  return fixups;
 }
 
-void test_no_fixups() {
-  puts("Testing near -- no fixups!");
+static void test_no_fixups() {
+  puts("Testing no fixups!");
   static uint8_t to_hook[]{ 0xf7, 0x0f, 0x1c, 0xf8, 0xf6, 0x57, 0x01, 0xa9, 0xf4, 0x4f, 0x02, 0xa9, 0xfd, 0x7b, 0x03,
                             0xa9, 0xfd, 0xc3, 0x00, 0x91, 0x48, 0x18, 0x40, 0xf9, 0x16, 0xd4, 0x42, 0xa9, 0xf3, 0x03,
                             0x02, 0xaa, 0xf4, 0x03, 0x01, 0xaa, 0x17, 0x01, 0x40, 0xf9, 0xe8, 0xba, 0x44, 0x39 };
-  perform_near_hook_test(to_hook);
-  puts("Testing far -- no fixups!");
-  perform_far_hook_test(to_hook);
+  {
+    TestWrapper init_hook(to_hook, "No fixups initial data");
+    init_hook.expect_opc(ARM64_INS_STR);
+    init_hook.expect_opc(ARM64_INS_STP);
+    init_hook.expect_opc(ARM64_INS_STP);
+    init_hook.expect_opc(ARM64_INS_STP);
+    init_hook.expect_opc(ARM64_INS_ADD);
+  }
+  {
+    auto results = perform_near_hook_test(to_hook);
+    TestWrapper fixup_validator(results.fixup_inst_destination.addr, "Near hook no fixups");
+    fixup_validator.expect_opc(ARM64_INS_STR);
+    fixup_validator.expect_opc(ARM64_INS_STP);
+    fixup_validator.expect_opc(ARM64_INS_STP);
+    fixup_validator.expect_opc(ARM64_INS_STP);
+    // Callback
+    fixup_validator.expect_b(&results.target.addr[4]);
+  }
+  {
+    auto results = perform_far_hook_test(to_hook);
+    TestWrapper fixup_validator(results.fixup_inst_destination.addr, "Far hook no fixups");
+    fixup_validator.expect_opc(ARM64_INS_STR);
+    fixup_validator.expect_opc(ARM64_INS_STP);
+    fixup_validator.expect_opc(ARM64_INS_STP);
+    fixup_validator.expect_opc(ARM64_INS_STP);
+    // Callback (ldr x17, DATA[0]; br x17)
+    fixup_validator.expect_ops<ARM64_OP_REG, ARM64_OP_IMM>(ARM64_INS_LDR, ARM64_REG_X17,
+                                                           (int64_t)&results.fixup_inst_destination.addr[6]);
+    fixup_validator.expect_ops<ARM64_OP_REG>(ARM64_INS_BR, ARM64_REG_X17);
+    // Data validation
+    // Check callback point is valid
+    fixup_validator.expect_big_data(reinterpret_cast<uint64_t>(&results.target.addr[4]));
+  }
 }
 
-void test_bls_tbzs_within_hook() {
-  puts("Testing near -- bls/tbzs");
+static void test_bls_tbzs_within_hook() {
+  puts("Testing bls/tbzs");
   static uint8_t to_hook[]{ 0x68, 0x00, 0x00, 0x37, 0xe0, 0x03, 0x17, 0xaa, 0x52, 0x3e, 0xfd, 0x97,
                             0xe0, 0x03, 0x17, 0xaa, 0x64, 0x7b, 0xfe, 0x97, 0x00, 0x00, 0x00, 0x00 };
-  perform_near_hook_test(to_hook);
-  puts("Testing far -- bls/tbzs");
-  perform_far_hook_test(to_hook);
+  {
+    TestWrapper init_hook(to_hook, "bls/tbzs");
+    init_hook.expect_ops<ARM64_OP_REG, ARM64_OP_IMM, ARM64_OP_IMM>(ARM64_INS_TBNZ, ARM64_REG_W8, 0,
+                                                                   (int64_t)&init_hook.data[3]);
+    init_hook.expect_opc(ARM64_INS_MOV);
+    init_hook.expect_opc(ARM64_INS_BL);
+    init_hook.expect_opc(ARM64_INS_MOV);
+    init_hook.expect_opc(ARM64_INS_BL);
+  }
+  {
+    auto results = perform_near_hook_test(to_hook);
+    TestWrapper fixup_validator(results.fixup_inst_destination.addr, "Near hook bls/tbzs");
+    fixup_validator.expect_ops<ARM64_OP_REG, ARM64_OP_IMM, ARM64_OP_IMM>(
+        ARM64_INS_TBNZ, ARM64_REG_W8, 0, (int64_t)&results.fixup_inst_destination.addr[3]);
+    fixup_validator.expect_opc(ARM64_INS_MOV);
+    fixup_validator.expect_opc(ARM64_INS_BL);
+    fixup_validator.expect_opc(ARM64_INS_MOV);
+    // Callback
+    fixup_validator.expect_b(&results.target.addr[4]);
+  }
+  {
+    auto results = perform_far_hook_test(to_hook);
+    TestWrapper fixup_validator(results.fixup_inst_destination.addr, "Far hook bls/tbzs");
+    // tbnz is still close, should still be emitted, but should still point to the mov, which is at idx 4
+    fixup_validator.expect_ops<ARM64_OP_REG, ARM64_OP_IMM, ARM64_OP_IMM>(
+        ARM64_INS_TBNZ, ARM64_REG_W8, 0, (int64_t)&results.fixup_inst_destination.addr[4]);
+    // mov is the same
+    fixup_validator.expect_opc(ARM64_INS_MOV);
+    // bl should turn into an ldr x17, DATA[0]; blr x17
+    fixup_validator.expect_ops<ARM64_OP_REG>(ARM64_INS_LDR, ARM64_REG_X17);
+    fixup_validator.expect_ops<ARM64_OP_REG>(ARM64_INS_BLR, ARM64_REG_X17);
+    fixup_validator.expect_opc(ARM64_INS_MOV);
+    // Callback (ldr x17, DATA[1]; br x17)
+    fixup_validator.expect_ops<ARM64_OP_REG, ARM64_OP_IMM>(ARM64_INS_LDR, ARM64_REG_X17,
+                                                           (int64_t)&results.fixup_inst_destination.addr[9]);
+    fixup_validator.expect_ops<ARM64_OP_REG>(ARM64_INS_BR, ARM64_REG_X17);
+    // Data validation
+    // The branch destination should be -0xB06B0 relative to the start of the hook.
+    // Thus: target == reinterpret_cast<uint64_t>(&
+    fixup_validator.expect_big_data(reinterpret_cast<uint64_t>(results.target.addr.data()) - 0xB06B0);
+    // Check callback point is valid
+    fixup_validator.expect_big_data(reinterpret_cast<uint64_t>(&results.target.addr[4]));
+  }
 }
 
-void test_ldr_ldrb_tbnz_bl() {
-  puts("Testing near -- ldr/ldrb/tbnz/bl");
+static void test_ldr_ldrb_tbnz_bl() {
+  puts("Testing ldr/ldrb/tbnz/bl");
   static uint8_t to_hook[]{ 0x17, 0x01, 0x40, 0xf9, 0xe8, 0xba, 0x44, 0x39, 0x68, 0x00, 0x00, 0x37,
                             0xe0, 0x03, 0x17, 0xaa, 0x52, 0x3e, 0xfd, 0x97, 0x00, 0x00, 0x00, 0x00 };
-  perform_near_hook_test(to_hook);
-  puts("Testing far -- ldr/ldrb/tbnz/bl");
-  perform_far_hook_test(to_hook);
+  {
+    TestWrapper init_hook(to_hook, "ldr/ldrb/tbnz/bl");
+    init_hook.expect_opc(ARM64_INS_LDR);
+    init_hook.expect_opc(ARM64_INS_LDRB);
+    init_hook.expect_ops<ARM64_OP_REG, ARM64_OP_IMM, ARM64_OP_IMM>(ARM64_INS_TBNZ, ARM64_REG_W8, 0,
+                                                                   (int64_t)&init_hook.data[5]);
+    init_hook.expect_opc(ARM64_INS_MOV);
+  }
+  {
+    auto results = perform_near_hook_test(to_hook);
+    TestWrapper fixup_validator(results.fixup_inst_destination.addr, "Near hook ldr/ldrb/tbnz/bl");
+    fixup_validator.expect_opc(ARM64_INS_LDR);
+    fixup_validator.expect_opc(ARM64_INS_LDRB);
+    // TBNZ should jump over the following instruction if taken
+    // TODO: This test should change once we support near tbz/tbnz optimizations
+    fixup_validator.expect_ops<ARM64_OP_REG, ARM64_OP_IMM, ARM64_OP_IMM>(
+        ARM64_INS_TBNZ, ARM64_REG_W8, 0, (int64_t)&results.fixup_inst_destination.addr[4]);
+    // B instruction should jump to skip the following far branch call
+    fixup_validator.expect_b(&results.fixup_inst_destination.addr[6]);
+    // Far branch call is given by an ldr x17, DATA[0]; br x17
+    fixup_validator.expect_ops<ARM64_OP_REG, ARM64_OP_IMM>(ARM64_INS_LDR, ARM64_REG_X17,
+                                                           (int64_t)&results.fixup_inst_destination.addr[8]);
+    fixup_validator.expect_ops<ARM64_OP_REG>(ARM64_INS_BR, ARM64_REG_X17);
+    fixup_validator.expect_opc(ARM64_INS_MOV);
+    // Callback
+    fixup_validator.expect_b(&results.target.addr[4]);
+    // Data validation
+    // Branch destination for tbnz taken should be hook[5]
+    fixup_validator.expect_big_data((uint64_t)&results.target.addr[5]);
+  }
+  {
+    auto results = perform_far_hook_test(to_hook);
+    TestWrapper fixup_validator(results.fixup_inst_destination.addr, "Far hook ldr/ldrb/tbnz/bl");
+    fixup_validator.expect_opc(ARM64_INS_LDR);
+    fixup_validator.expect_opc(ARM64_INS_LDRB);
+    // TBNZ should jump over the following instruction if taken
+    fixup_validator.expect_ops<ARM64_OP_REG, ARM64_OP_IMM, ARM64_OP_IMM>(
+        ARM64_INS_TBNZ, ARM64_REG_W8, 0, (int64_t)&results.fixup_inst_destination.addr[4]);
+    // B instruction should jump to skip the following far branch call
+    fixup_validator.expect_b(&results.fixup_inst_destination.addr[6]);
+    // Far branch call is given by an ldr x17, DATA[0]; br x17
+    fixup_validator.expect_ops<ARM64_OP_REG, ARM64_OP_IMM>(ARM64_INS_LDR, ARM64_REG_X17,
+                                                           (int64_t)&results.fixup_inst_destination.addr[9]);
+    fixup_validator.expect_ops<ARM64_OP_REG>(ARM64_INS_BR, ARM64_REG_X17);
+    fixup_validator.expect_opc(ARM64_INS_MOV);
+    // Callback (ldr x17, DATA[1]; br x17)
+    fixup_validator.expect_ops<ARM64_OP_REG, ARM64_OP_IMM>(ARM64_INS_LDR, ARM64_REG_X17,
+                                                           (int64_t)&results.fixup_inst_destination.addr[11]);
+    fixup_validator.expect_ops<ARM64_OP_REG>(ARM64_INS_BR, ARM64_REG_X17);
+    // Data validation
+    // Branch destination for tbnz taken should be hook[5]
+    fixup_validator.expect_big_data((uint64_t)&results.target.addr[5]);
+    // Check callback point is valid
+    fixup_validator.expect_big_data(reinterpret_cast<uint64_t>(&results.target.addr[4]));
+  }
 }
 
-void test_adrp() {
-  puts("Testing near -- adrp");
+static void test_adrp() {
+  puts("Testing adrp");
   static uint8_t to_hook[]{ 0x09, 0x00, 0x00, 0x90, 0xa8, 0x00, 0x80, 0x52, 0x28, 0x01,
                             0x00, 0xb9, 0x28, 0x01, 0x00, 0xb9, 0xc0, 0x03, 0x5f, 0xd6 };
-  perform_near_hook_test(to_hook);
-  puts("Testing far -- adrp");
-  perform_far_hook_test(to_hook);
+  {
+    TestWrapper init_hook(to_hook, "adrp");
+    init_hook.expect_ops<ARM64_OP_REG, ARM64_OP_IMM>(ARM64_INS_ADRP, ARM64_REG_X9, (int64_t)(&to_hook[0]) & ~0xfff);
+    init_hook.expect_opc(ARM64_INS_MOV);
+    init_hook.expect_opc(ARM64_INS_STR);
+    init_hook.expect_opc(ARM64_INS_STR);
+    init_hook.expect_opc(ARM64_INS_RET);
+  }
+  {
+    auto results = perform_near_hook_test(to_hook);
+    TestWrapper fixup_validator(results.fixup_inst_destination.addr, "Near hook adrp");
+    // ADRP is replaced with an ldr to load the data directly
+    // LDR x9, DATA[0]
+    fixup_validator.expect_ops<ARM64_OP_REG, ARM64_OP_IMM>(ARM64_INS_LDR, ARM64_REG_X9,
+                                                           (int64_t)&results.fixup_inst_destination.addr[5]);
+    fixup_validator.expect_opc(ARM64_INS_MOV);
+    fixup_validator.expect_opc(ARM64_INS_STR);
+    fixup_validator.expect_opc(ARM64_INS_STR);
+    // Callback
+    fixup_validator.expect_b(&results.target.addr[4]);
+    // Data validation
+    // ADRP result must match
+    fixup_validator.expect_big_data((int64_t)(&to_hook[0]) & ~0xfff);
+  }
+  {
+    auto results = perform_far_hook_test(to_hook);
+    TestWrapper fixup_validator(results.fixup_inst_destination.addr, "Far hook adrp");
+    // ADRP is replaced with an ldr to load the data directly
+    // LDR x9, DATA[0]
+    fixup_validator.expect_ops<ARM64_OP_REG, ARM64_OP_IMM>(ARM64_INS_LDR, ARM64_REG_X9,
+                                                           (int64_t)&results.fixup_inst_destination.addr[6]);
+    fixup_validator.expect_opc(ARM64_INS_MOV);
+    fixup_validator.expect_opc(ARM64_INS_STR);
+    fixup_validator.expect_opc(ARM64_INS_STR);
+    // Callback (ldr x17, DATA[1]; br x17)
+    fixup_validator.expect_ops<ARM64_OP_REG, ARM64_OP_IMM>(ARM64_INS_LDR, ARM64_REG_X17,
+                                                           (int64_t)&results.fixup_inst_destination.addr[8]);
+    fixup_validator.expect_ops<ARM64_OP_REG>(ARM64_INS_BR, ARM64_REG_X17);
+    // ADRP result must match
+    fixup_validator.expect_big_data((int64_t)(&to_hook[0]) & ~0xfff);
+    // Check callback point is valid
+    fixup_validator.expect_big_data(reinterpret_cast<uint64_t>(&results.target.addr[4]));
+  }
 }
 
 // TODO: Test a case where we have a loop in the first 4 instructions
@@ -135,4 +304,5 @@ int main() {
   test_bls_tbzs_within_hook();
   test_ldr_ldrb_tbnz_bl();
   test_adrp();
+  puts("ALL GOOD!");
 }
---
+++ test/test-wrapper.hpp
@@ -0,0 +1,147 @@
+#pragma once
+
+#include <fmt/compile.h>
+#include <fmt/core.h>
+#include <fmt/format.h>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <span>
+#include <string>
+#include <string_view>
+#include <tuple>
+#include <utility>
+#include "capstone/arm64.h"
+#include "capstone/capstone.h"
+
+#include "../shared/fixups.hpp"
+
+// TODO: Only do a dump if an error happens
+// Dump should do a normal decode loop:
+// - Dump the original instructions for the hook
+// - Dump the trampoline
+// - Dump the new hook
+// Perhaps we make TestWrapper take a Fixups instance for this? That way we can do all three of those.
+#define ERROR(S, ...)                              \
+  fmt::print(stderr, FMT_COMPILE(S), __VA_ARGS__); \
+  fmt::print(stderr, "\n");                        \
+  std::exit(1);
+
+// Helper construct to validate data from a hooked target
+struct TestWrapper {
+  std::span<uint32_t> data;
+  uint32_t idx{ 0 };
+  std::string test_name;
+  TestWrapper(std::span<uint32_t> bytes, std::string_view test) : data(bytes), test_name(test) {
+    start_test();
+  }
+  TestWrapper(std::span<uint8_t> bytes, std::string_view test)
+      : data(std::span<uint32_t>(reinterpret_cast<uint32_t*>(&bytes[0]),
+                                 reinterpret_cast<uint32_t*>(&bytes[bytes.size()]))),
+        test_name(test) {
+    start_test();
+  }
+  ~TestWrapper() {
+    fmt::print("---Passed test: {}\n", test_name);
+    fflush(stdout);
+  }
+
+  void start_test() const {
+    fmt::print("---Starting test: {}\n", test_name);
+    fflush(stdout);
+  }
+
+  cs_insn* get_next() {
+    auto handle = flamingo::getHandle();
+    cs_insn* insns = nullptr;
+    auto count = cs_disasm(handle, reinterpret_cast<uint8_t const*>(&data[idx]), sizeof(uint32_t),
+                           reinterpret_cast<uint64_t>(&data[idx]), 1, &insns);
+    idx++;
+    if (count == 1) {
+      // We just leak this, who cares.
+      return insns;
+    }
+    // Failed to disassemble!
+    return nullptr;
+  }
+  uint32_t get_next_data() {
+    return data[idx++];
+  }
+  uint64_t get_next_big_data() {
+    uint64_t value = static_cast<uint64_t>(get_next_data());
+    value |= static_cast<uint64_t>(get_next_data()) << 32U;
+    return value;
+  }
+
+  void expect_inst_opc(cs_insn* inst, unsigned int opcode) const {
+    if (inst == nullptr) {
+      ERROR("Mismatched instruction at index: {}\n Expected opcode: {}\n Got: Invalid instruction", idx - 1, opcode);
+    }
+    if (inst->id != opcode) {
+      ERROR("Mismatched instruction at index: {}\n Expected opcode: {}\n Got: {}", idx - 1, opcode, inst->id);
+    }
+  }
+  void expect_opc(unsigned int opcode) {
+    auto inst = get_next();
+    expect_inst_opc(inst, opcode);
+    cs_free(inst, 1);
+  }
+  void expect_b(uint32_t* addr) {
+    auto inst = get_next();
+    expect_inst_opc(inst, ARM64_INS_B);
+    if (inst->detail->arm64.operands[0].imm != reinterpret_cast<int64_t>(addr)) {
+      ERROR("Mismatched B at index: {}\n Expected immediate: {}\n Got: {:#x}", idx - 1, fmt::ptr(addr),
+            inst->detail->arm64.operands[0].imm);
+    }
+    cs_free(inst, 1);
+  }
+  template <arm64_op_type OpType, class T>
+  static bool compare_op(cs_arm64_op const& op, T value) {
+    if (op.type != OpType) return false;
+    switch (OpType) {
+      case arm64_op_type::ARM64_OP_IMM:
+        return op.imm == value;
+      case arm64_op_type::ARM64_OP_REG:
+        return op.reg == value;
+      default:
+        ERROR("Cannot compare operand of type: {}", static_cast<int>(OpType));
+    }
+  }
+  template <arm64_op_type... OpTypes, size_t... Sz, class... TArgs>
+  void validate_ops(std::index_sequence<Sz...>, cs_arm64_op* ops, std::tuple<TArgs...> expected) {
+    auto expect_op = [&]<arm64_op_type OpType, size_t Size>() {
+      if (!compare_op<OpType>(ops[Size], std::get<Size>(expected))) {
+        ERROR("Mismatched instruction at index: {} Mismatched operand at index: {}\n Expected: {}: {}\n Got: {}: {}",
+              idx, Size, static_cast<int64_t>(OpType), static_cast<int64_t>(std::get<Size>(expected)),
+              static_cast<int64_t>(ops[Size].type), ops[Size].imm);
+      }
+    };
+    (expect_op.template operator()<OpTypes, Sz>(), ...);
+  }
+  template <arm64_op_type... OpTypes, class... TArgs>
+  void expect_ops(unsigned int opcode, TArgs&&... args) {
+    auto inst = get_next();
+    expect_inst_opc(inst, opcode);
+    auto opcount = inst->detail->arm64.op_count;
+    static_assert(sizeof...(OpTypes) == sizeof...(TArgs), "Must have a type for each operand");
+    if (sizeof...(args) > opcount) {
+      ERROR("Mismatched instruction at index: {}\n Expected opcount: {}\n Got: {}", idx - 1, sizeof...(args), opcount);
+    }
+    // Index wrapper to validate each operand in order
+    validate_ops<OpTypes...>(std::make_index_sequence<sizeof...(args)>{}, inst->detail->arm64.operands,
+                             std::make_tuple(std::forward<TArgs>(args)...));
+    cs_free(inst, 1);
+  }
+  void expect_data(uint32_t expected) {
+    auto data = get_next_data();
+    if (data != expected) {
+      ERROR("Mismatched 32b data at index: {}\n Expected: {}\n Got: {}", idx - 1, expected, data);
+    }
+  }
+  void expect_big_data(uint64_t expected) {
+    auto data = get_next_big_data();
+    if (data != expected) {
+      ERROR("Mismatched 64b data at index: {}\n Expected: {}\n Got: {}", idx - 1, expected, data);
+    }
+  }
+};
