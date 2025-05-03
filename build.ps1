3b6dc7539dc7f2eb0c17de8095cb722e12a0dc38
Adam ? <adamznow@gmail.com>
2025-05-02 22:50:48 -0700

    fixup! Add android install, fixup cmakelists a bit

--- build.ps1
+++ build.ps1
@@ -12,36 +12,14 @@ function Clean-Build-Folder {
 $NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
 
 # Clean-Build-Folder
-# build tests
-
-& cmake -G "Ninja" -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DTEST_BUILD=1 -B build
+# Build flamingo (not as tests) as tests are built via different cmake line (-DTEST_BUILD=1, or unspecified)
+& cmake -G "Ninja" -DCMAKE_BUILD_TYPE="RelWithDebInfo" -DTEST_BUILD=0 -B build
 & cmake --build ./build
 
 $ExitCode = $LastExitCode
-
 
 if (-not ($ExitCode -eq 0)) {
     $msg = "ExitCode: " + $ExitCode
     Write-Output $msg
     exit $ExitCode
 }
-
-# # clean folder
-# Clean-Build-Folder
-# # build mod
-
-# & cmake -G "Ninja" -DCMAKE_BUILD_TYPE="RelWithDebInfo" -B build
-# & cmake --build ./build
-
-# $ExitCode = $LastExitCode
-
-# Post build, we actually want to transform the compile_commands.json file such that it has no \\ characters and ONLY has / characters
-# (Get-Content -Path build/compile_commands.json) |
-#     ForEach-Object {$_ -Replace '\\\\', '/'} | Set-Content -Path build/compile_commands.json
-
-# To build tests, we just compile with our local clang++ into an executable
-# Kind of wacky but will work on linux
-# Requires libcapstone-dev installed
-# sudo apt install 
-# clang++ test/main.cpp src/trampoline.cpp src/trampoline-allocator.cpp -o build/test -std=c++20 -Ishared -Iextern/includes -lcapstone -Iextern/includes/fmt/fmt/include -L/usr/lib/x86_64-linux-gnu -DFMT_HEADER_ONLY -Wall -Wextra -Werror -g
-exit $ExitCode
