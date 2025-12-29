8f5f1e7b88d93f7d2063a4e7086ffe2846e777be
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2025-12-29 18:41:07 -0400

    Update build configuration for test environment by adding build_test directory in .gitignore and modifying test script

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euo pipefail
-CC=clang CXX=clang cmake -B build -DTEST_BUILD=1 -GNinja
-ninja -C build
-ctest --test-dir build --output-on-failure
+CC=clang CXX=clang cmake -B build_test -DTEST_BUILD=1 -GNinja
+ninja -C build_test
+ctest --test-dir build_test --output-on-failure
